8306fc0d89e53689060e95a6d2fc0b44c60f74b7
RUBEN <RUBEN@RUBEN-PC.unal.loc>
2016-02-24 10:37:12 -0500

    casifinal

--- src/problema1/Problema1.java
+++ src/problema1/Problema1.java
@@ -15,14 +15,14 @@ public class Problema1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      java.util.Scanner num= new java.util. Scanner(System.in);
-      int p,s;
-      System.out.println ("ingrese el numero de municipios");
-       p = num.nextInt();
-       System.out.println ("ingrese el numero de candidatos");
-       s = num.nextInt();
-       int [][] Votos = new int [p][s];
-       for( int i = 0; i <p; i++){
+              java.util.Scanner num= new java.util. Scanner(System.in);
+        int p,s;
+        System.out.println ("ingrese el numero de municipios");
+        p = num.nextInt();
+        System.out.println ("ingrese el numero de candidatos");
+        s = num.nextInt();
+        int [][] Votos = new int [p][s];
+        for( int i = 0; i <p; i++){
           for(int q=0;q<s;q++){
           System.out.print(" ingrese el numero de votos del municipio"+(i+1));
           System.out.println("para el candidato "+(q+1));
@@ -30,26 +30,43 @@ public class Problema1 {
           Votos[i][q]=r; 
           
           }}
-       for( int b = 0; b <p; b++){
-          for(int k=0;k<s;k++){
-                 System.out.print("votos del candidato "+(b+1)+ " para el municipio "+ (b+1)+ ":");
-              System.out.print(Votos[b][k]+ "\n");
-          }}
-       int mayor=0;
-       int ganador=0;
-      for(int c=0;c<s;c++){
-          int suma=0; 
-            for(int x=0;x<p;x++){
-        suma= Votos [x][c]+suma;
-            }
-        System.out.print("el total de votos para el candidato "+(c+1) );
-        System.out.println("es : "+suma);
-        if (suma>mayor){
+        for( int b = 0; b <p; b++){
+              for(int k=0;k<s;k++){                
+              System.out.print(Votos[b][k]+ "\t");
+                }
+       System.out.println();
+       }
+       double porcentaje;
+    int total1=0;       
+     
+        int mayor=0;
+        int ganador=0;  
+        int total=0;
+        int sumcandidatos []=new int [s];
+      for(int c=0;c<s;c++){         
+        int suma=0;            
+          for(int x=0;x<p;x++){
+              suma= Votos [x][c]+suma;        
+          }
+            total=suma+total;       
+            System.out.print("el total de votos para el candidato "+ ""+(c+1)+"es :"+suma );        
+        if (mayor<suma){
             mayor=suma;
-            ganador=c;
-         }
-            }
-        System.out.println ("El ganador es " +ganador);
-    }
-    
+            ganador=c;            
+         } 
+        //porcentaje = (suma*100)/total;
+        sumcandidatos [c]=suma;
+          
+      }
+      
+        for(int u=0;u<s;u++){
+            porcentaje=(sumcandidatos[u]*100)/total;
+            System.out.println("El porcentaje del candidato "+ (u+1)+"es "+ porcentaje+"%");
+        }
+        System.out.println ("El ganador es el candidato: " +(ganador+1));
+        System.out.println ("El total de votos de todos los candidatos es: " +total);          
+          
+            }   
+     
 }
+
